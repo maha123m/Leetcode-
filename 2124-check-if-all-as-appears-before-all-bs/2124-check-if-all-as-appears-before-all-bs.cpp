3d91271e80a093bd1025bbf3f84fcc792bb8cd8d
maha_mali <99613493+maha123m@users.noreply.github.com>
2023-11-18 11:32:45 +0200

    Time: 3 ms (42.98%), Space: 6.4 MB (88.60%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     bool checkString(string s) {
         int n=s.length();
-        int count=0;
         for(int i=0;i<n-1;i++){
-            if ((s[i]=='a' && s[i+1]=='a')||(s[i]=='a' && s[i+1]=='b')||(s[i]=='b' && s[i+1]=='b')){
-                count++;
+            if ((s[i]=='b' && s[i+1]=='a')){
+               return false;
             }
         }
-        if (count==n-1){
+        
             return true;
-        }
-        else
-            return false;
         
     }
 };
