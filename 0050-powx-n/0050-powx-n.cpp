11799e7cff945c13cf8a33a3a9095db05ce01d74
maha_mali <99613493+maha123m@users.noreply.github.com>
2023-11-21 13:19:45 +0200

    Time: 3 ms (42.07%), Space: 6.4 MB (59.67%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-            if (n == INT_MIN) {
-                x = 1 / x;
-                return x * myPow(x, INT_MAX);  // Handle INT_MIN separately
-            }
-        else if (n < 0) {
-             x = 1 / x;
-            n = -n;
-        }
-        return power(x, n);
-    }
-
-private:
-    double power(double x, int n) {
         if (n == 0) {
             return 1.0;
         }
 
-        double halfPower = power(x, n / 2);
-
-        if (n % 2 == 0) {
-            // If n is even
-            return halfPower * halfPower;
-        } else {
-            // If n is odd
-            return x * halfPower * halfPower;
+        if (x == 1) {
+            return 1.0;
         }
+
+        if (x == -1) {
+            return (n % 2 == 0) ? 1.0 : -1.0;
+        }
+
+        if (n == INT_MIN) {
+            x = 1 / x;
+            n = INT_MAX;  // Handle the case when n is INT_MIN to avoid overflow
+        } else if (n < 0) {
+            n = -n;
+            x = 1 / x;
+        }
+
+        double ans = 1.0;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                ans *= x;
+            }
+            x *= x;
+            n /= 2;
+        }
+
+        return ans;
     }
-        
-  
 };
